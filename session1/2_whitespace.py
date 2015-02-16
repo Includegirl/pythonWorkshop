3dcb2736704459340edd9d8dff3d8b7776b1d3f5
Xiomara7 <figueroaxiomara7@gmail.com>
2015-02-16 12:29:52 -0400

    added comments

---
+++ session1/2_whitespace.py
@@ -0,0 +1,25 @@
+'''
+En python, el espacio es utilizado para estructurar el código. Así que 
+hay que ser bien cuidadosos cuando lo usamos. Más adelante lo veremos 
+mejor. 
+'''
+
+# COMENTARIOS
+
+''' 
+Para añadir texto que no es código y para evitar errores cuando 
+estemos corriendo nuestro códido, los comentarios 'como lo que 
+estamos usando para dar esta presentación' van dentro de caracteres
+específicos
+'''
+
+# COMENTARIO SENCILLO 
+
+# Para escribir comentarios sencillos usamos en #
+
+'''
+Para escribir comentarios en 
+Másde
+una
+linea
+usamos '''
