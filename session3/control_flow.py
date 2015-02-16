f73e16c37ac7a76c7b9ec0c89d1bd2079a1a7a0d
Xiomara7 <figueroaxiomara7@gmail.com>
2015-02-16 13:18:39 -0400

    started control flow (if else)

---
+++ session3/control_flow.py
@@ -0,0 +1,53 @@
+'''
+Vamos a empezar con el más simple de los conceptos: comparación!
+'''
+
+# COMPARADORES
+
+'''
+	Igual a 			(==)
+	No Igual 			(!=)
+	Menor que 			(<)
+	Menor o igual que 	(<=)
+	Mayor que 			(>)
+	Mayor o igual que 	(>=)
+
+'''
+
+##### EJERCICIO 1 #####
+
+# Asigna apropiadamente si es cierto o falso:
+
+# 17 < 328 
+bool_one = True   # Hicimos éste para ti!
+
+# 100 == (2 * 50) 
+# bool_two = 
+
+# 19 <= 19
+# bool_three = 
+
+#  -22 >= -18 
+# bool_four = 
+
+# 99 != (98 + 1) 
+# bool_five = 
+
+# CONDICIONALES
+
+'''
+'IF' Es una oración condicional que ejecuta un código después
+de haber verificado que la expresión anterios era cierta (TRUE)
+
+La sintaxis de un IF luce así:
+
+	if <una condicion>
+		# linea de codigo 1
+		# linea de codigo 2
+		# etcetera
+
+'''
+
+
+
+
