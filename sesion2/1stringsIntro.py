bd77cb29bf84ea3f3e3b2f186d77cb070debaf1b
Include Girls <include.girls.uprrp@gmail.com>
2015-02-16 10:04:56 -0400

    Update and rename stringsIntro.py to 1stringsIntro.py

---
+++ sesion2/1stringsIntro.py
@@ -0,0 +1,42 @@
+# Un 'string' esa cadena de caracteres la cual es un tipo de variable 
+# que puede representar números, letras y simbolos.
+# Ejemplos:
+
+cancion = "bacon Pancakes, making bacon Pancakes"
+numeros = "1234"
+comida  = "Ice cream sandwich!"
+# En el ejemplo anterior creamos una variable llamada
+# cancion y le asignamos el string "bacon Pancakes, making bacon Pancakes"
+
+##### EJERCICIO 1 #####
+# Crea una variable que se llame 'nombre'
+# y asígnale como string tu nombre
+
+
+##### EJERCICIO 2 #####
+# http://bit.ly/1BgZti4
+
+
+# Existen dos maneras de representar los 'strings', aquellos que tienen 
+# 'single quotes' y aquellos que tienen 'double quotes'
+# Ejemplo:
+
+# Double Quotes
+"¡Si llegaste a este punto, estamos orgullosas! :')"
+# Single Quotes
+'¡Si llegaste a este punto, estamos orgullosas! :')'
+# ¿Qué podemos notar con los 'single quotes'?
+
+# No todos los caracteres pueden ser representados con strings
+# por lo que utilizamos lo que se llama un 'escape character'
+# el cual ayuda a arreglar este problema. En python es: \
+# Ejemplo:
+# Sin el escape character
+'Someone told me that there's pizza for lunch!'
+# Con el escape character
+'Someone told me that there\'s pizza for lunch!'
+
+##### EJERCICIO 3 #####
+# Arregla el string que está en la linea 27
+
+
