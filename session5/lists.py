4a35450a1ecd13d7e3929cd6fc2afb88783a979a
Xiomara7 <figueroaxiomara7@gmail.com>
2015-02-16 13:23:02 -0400

    started session 5 about lists

---
+++ session5/lists.py
@@ -0,0 +1,17 @@
+'''
+Las listas son un 'data type' que podemos usar para guardar
+una coleccion de diferentes pedazos de información como una 
+secuencia debajo deun mismo nombre. 
+
+Asi luce una lista:
+
+lista = [elemento 1, elemento2, ...]
+
+Una lista puede estar vacia.
+
+lista_vacia = []
+
+'''
+
+
+
