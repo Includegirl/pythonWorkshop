d55b9637ba5816ff6bf98ce007a4d12f65a9dffc
Xiomara7 <figueroaxiomara7@gmail.com>
2015-02-16 12:59:41 -0400

    added calculator example

---
+++ session1/2_calculator.py
@@ -0,0 +1,32 @@
+'''
+Ahora aplicaremos todo lo aprendido!
+
+Suponga que hemos terminado de comer en un restaurante y recibes esta
+factura:
+
+	costo de la comida: $44.50
+	TAX: $6.75
+	Propina: 15%
+
+Vamos a hacer una calculadora para saber cuánto es de propina!
+
+'''
+
+##### EJERCICIO 1 #####
+
+# Declaremos una variable llamada 'comida' y asignemosle 44.50
+
+comida = 44.50
+
+# Ahora creemos una variable para el TAX HINT: Tenemos que dividir 
+# 6.75 / 100 para sacar el valor decimal y la computadora nos entienda!
+
+# tax = ?
+
+# Ahora creemos una variable para el porcentaje de propina! HINT: Tenemos 
+# que dividir 15.0 / 100 para sacar el valor decimal de la propina!
+
+# propina = ?
+
+
+
