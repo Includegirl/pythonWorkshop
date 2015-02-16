a3ce507261b072df1af0f9d4ce1c2a121cf54487
Xiomara7 <figueroaxiomara7@gmail.com>
2015-02-16 12:49:15 -0400

    merge all session 1 in one file

---
+++ session1/1_syntax.py
@@ -0,0 +1,94 @@
+''' 
+Python es un lenguaje de programación fácil de aprender. Puedes usarlo 
+para crear páginas web, aplicaciones, juegos y hasta un 'search engine' 
+'''
+
+# Variables 
+
+''' 
+Una variable guarda un pedazo de información y le da un nombre específico 
+para poder accesarla luego. 
+
+Por ejemplo: python = 1 
+'''
+
+##### EJERCICIO 1 (tentativo) ###
+
+'''
+Crea una variable con el nombre de 'mi_variable' y asignale el valor de 10
+'''
+
+# Booleanas
+
+'''
+Otro 'data type' lo son los booleanos. Un booleano es como un switch de luz, 
+que solo puede estar prendido o apagado. Un booleano solo puede ser 'cierto' 
+o 'falso'
+
+Por ejemplo: a = True
+
+'''
+
+##### EJERCICIO 2 (tentativo) #####
+
+'''
+Crea una variable con el nombre de 'my_bool' y asignale un valor posible
+'''
+
+# ESPACIOS
+
+'''
+En python, el espacio es utilizado para estructurar el código. Así que 
+hay que ser bien cuidadosos cuando lo usamos. Más adelante lo veremos 
+mejor. 
+'''
+
+# COMENTARIOS
+
+''' 
+Para añadir texto que no es código y para evitar errores cuando 
+estemos corriendo nuestro códido, los comentarios 'como lo que 
+estamos usando para dar esta presentación' van dentro de caracteres
+específicos
+'''
+
+# COMENTARIO SENCILLO 
+
+# Para escribir comentarios sencillos usamos en #
+
+'''
+Para escribir comentarios en 
+Másde
+una
+linea
+usamos '''
+
+
+# MATEMATICAS
+
+'''
+OK! Ahora haremos un poco de matemáticas! Podemos sumar, restar, 
+multiplicar y dividir numeros en python de la siguiente manera:
+'''
+
+# EJEMPLOS
+
+suma = 23 + 12
+resta = 43 - 10
+multiplicacion = 2 * 10
+division = 10 / 5
+
+'''
+Por qué hacer matemáticas en python si cualquier calculadora lo hace?
+Porque podemos combinarla con otros 'data types' ejemplo: booleanos
+y comandos para crear programas útiles! 
+'''
+
+# EXPONENTE
+
+# OPERACIONES MATEMATICAS
+
+
+
+
+
