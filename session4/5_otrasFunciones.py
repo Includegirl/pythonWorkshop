fcb2f7df535f5c823992159c3e0886bca96c9fea
Include Girls <include.girls.uprrp@gmail.com>
2015-02-22 19:03:25 -0400

    Update and rename session4 to session4/5_otrasFunciones.py

---
+++ session4/5_otrasFunciones.py
@@ -0,0 +1,21 @@
+# Otras funciones que podemos utilizar
+# que vienen incorporadas en el lenguaje
+# sin la necesidad de importar módulos.
+
+# max(a, b, c, ...)
+# Esta función va a devolver el numero mayor entre a, b, c, ...
+# min(a, b, c, ...)
+# Esta función va a devolver el numero menor entre a, b, c, ...
+#abs(-a)
+# Esta función devuelve el número absoluto de a
+#type(a)
+# Esta función devuelve el tipo de la variable a que le estamos pasando, entre los tipos están (int, float y str)
+
+#Ejemplos:
+print max(3, 90, 10, -80)
+print min(3, 90, 10, -80)
+print abs(10)
+print abs(-10)
+print type("hola")
+print type(9)
+print type(9.0)
