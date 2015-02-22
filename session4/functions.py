2d784fbf17987ebd3c65bed50ec9fa2ffb079b71
Include Girls <include.girls.uprrp@gmail.com>
2015-02-22 17:31:19 -0400

    Create functions.py

---
+++ session4/functions.py
@@ -0,0 +1,38 @@
+#Una función nos permite reusar piezas de código
+
+##### EJERCICIO 1 #####
+# Corre el siguiente código:
+#bit.ly ...
+
+# Discute con tus compañeros en la mesa
+# qué pueden entender del código 
+
+# Sintaxis de una función
+#     La cabecera ('header') de una función, la cual incluye
+#     la pralabra reservada 'def', el nombre de la función,
+#     y los parámetros que la función requiere.
+#     Lo siguiente es un ejemplo:
+
+def hello_world(): # No hay parámetros
+# Opcional: Un comentario que explica qué hace la función
+"""Prints 'Hello World!' to the console."""
+
+# El cuerpo de la función la cual describe
+#los procedimientos que la función hace. 
+# El cuerpo es indentado.
+
+print "Hello World!"
+
+# Aqui está el ejemplo de la función completa:
+### Este ejemplo es tomado del curso de Codecademy directamente ###
+
+def hello_world():
+    """Prints 'Hello World!' to the console."""
+    print "Hello World!"
+
+##### Ejercicio 2 #####
+''' Instructions
+Crea una función, llamada 'spam', que imprime el string "Pizza!" 
+en la consola. Recuerda crear un comentario que describa
+lo que hace la función
+'''
