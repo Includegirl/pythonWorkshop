712a18c842cb522247b5d6556eeab1ec5f233835
Include Girls <include.girls.uprrp@gmail.com>
2015-02-22 17:48:40 -0400

    Update and rename session4 to session4/call.py

---
+++ session4/call.py
@@ -0,0 +1,23 @@
+# Despues de definir una función, la misma
+# debe ser invocada para que ejecute lo
+# que se le asignó a hacer
+
+# En el ejercicio anterior creamos una función
+# llamada 'spam' ahora vamos a llamarla 
+# para que ejecute su trabajo, colocando
+# 'spam()' en la ultima linea justo despues
+# de la última linea de la función.
+
+#### Ejercicio 1 ####
+# Haga el llamado de la siguiente función
+# e incluya el número 10 entre los paréntesis.
+
+def square(n):
+    """Returns the square of a number."""
+    squared = n**2
+    print "%d squared is %d." % (n, squared)
+    return squared
+# Llamado
+      
+
+
